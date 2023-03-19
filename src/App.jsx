96202dcb6004c1e19c59ee98a15cd53fd9c155ae
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-03-19 11:04:33 -0300

    render movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import './App.css'
+import withResults from './mocks/with-results.json'
+// import withoutResults from './mocks/no-results.json'
+
 function App () {
+  const movies = withResults.Search
+  const hasMovies = movies?.length > 0
+
   return (
-    <div>
-      <h1>Buscador de películas</h1>
+    <div className='page'>
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form'>
+          <input placeholder='Avengers, Star Wars, The matrix...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+      <main>
+        {
+          hasMovies
+            ? (<ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+                  </li>))
+              }</ul>)
+            : (<p>No se encontraron películas para esta búsqueda</p>)
+        }
+      </main>
     </div>
   )
 }
