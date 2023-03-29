9034f8e753d0b65e7b9f7247b9a3267a9ff7adf0
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-03-29 09:32:05 -0300

    image galery

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,7 @@ function App () {
       <header>
         <nav>
           <h1 className='showcase'>SHOWCASE</h1>
+          <div style={{ flexGrow: 1 }} />
           <form className='form' onSubmit={handleSubmit}>
             <input
               style={{ border: '1px solid transparent', borderColor: error ? 'red' : 'transparent' }}
@@ -57,10 +58,27 @@ function App () {
       </header>
       <div style={{ minHeight: 70 }} />
       <main>
-        {
-          loading ? <p>Cargando... </p> : <Movies movies={movies} />
-        }
-        <Movies movies={favorites} />
+        <h1>Search</h1>
+        <div className='catalog'>
+          {
+            loading ? <div className='loader' /> : <Movies movies={movies} />
+          }
+        </div>
+        <h1>Trending</h1>
+        <div className='catalog'>
+          <section>
+            <img src='https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg' alt='Blade Runner 2020' />
+            <img src='https://m.media-amazon.com/images/M/MV5BN2Y2OWU4MWMtNmIyMy00YzMyLWI0Y2ItMTcyZDc3MTdmZDU4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg' alt='12 Monkeys' />
+            <img src='https://m.media-amazon.com/images/M/MV5BMTkzMzA1OTI3N15BMl5BanBnXkFtZTYwMzUyMDg5._V1_SX300.jpg' alt='Equilibrium' />
+            <img src='https://m.media-amazon.com/images/M/MV5BNTE1OWI1YzgtZjEyMy00MjQ4LWE0NWMtYTNhYjc0ZDQ3ZGRkXkEyXkFqcGdeQXVyNDQ2MTMzODA@._V1_SX300.jpg' alt='The Book of Eli' />
+            <img src='https://m.media-amazon.com/images/M/MV5BZTM2ZGJmNjQtN2UyOS00NjcxLWFjMDktMDE2NzMyNTZlZTBiXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg' alt='American Psycho' />
+            <img src='https://m.media-amazon.com/images/M/MV5BNzU1MDI1NDM1NF5BMl5BanBnXkFtZTcwMzU5OTkyMQ@@._V1_SX300.jpg' alt='Land of the Dead' />
+          </section>
+        </div>
+        <h1>Favorites</h1>
+        <div className='catalog'>
+          <Movies movies={favorites} />
+        </div>
       </main>
     </div>
   )
